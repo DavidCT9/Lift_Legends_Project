ec322f0f2c9822c4c4f440c0341f5a747d389f24
eselemu <emi.f.cass@gmail.com>
2024-09-26 18:55:06 -0600

    +Database Schema

--- server/server.js
+++ server/server.js
@@ -1,8 +1,8 @@
-import express from 'express';
-import cors from 'cors';
-import mongoose from 'mongoose';
-import bodyParser from 'body-parser';
-import dotenv from 'dotenv';
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
 
 dotenv.config();
 
@@ -19,49 +19,90 @@ const mongoUrl = `mongodb+srv://${mongoUser}:${mongoPassword}@cluster0.6iz2suz.m
 
 console.log(mongoUrl);
 // Connect to MongoDB
-mongoose.connect(mongoUrl)
-  .then(() => console.log("Connected to MongoDB"))
-  .catch((error) => console.error("MongoDB connection error:", error));
+mongoose
+	.connect(mongoUrl)
+	.then(() => console.log("Connected to MongoDB"))
+	.catch((error) => console.error("MongoDB connection error:", error));
+
+const ExercisesPointsSchema = new mongoose.Schema({
+	deadlift: { type: Number, required: true },
+	benchPress: { type: Number, required: true },
+	squat: { type: Number, required: true },
+	shoulderPress: { type: Number, required: true },
+	barbellRow: { type: Number, required: true },
+	bicepCurl: { type: Number, required: true },
+	latPulldown: { type: Number, required: true },
+	lateralRaise: { type: Number, required: true },
+	tricepExtension: { type: Number, required: true },
+	legPress: { type: Number, required: true },
+});
+const ExercisesPoints = mongoose.model(
+	"ExercisesPoints",
+	ExercisesPointsSchema
+);
 
 // Define User Schema
 const UserSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  username: { type: String, required: true, unique: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
+	name: { type: String, required: true },
+	username: { type: String, required: true, unique: true },
+	email: { type: String, required: true, unique: true },
+	password: { type: String, required: true },
+	currentAvatar: { type: Number, required: true },
+	avatars: { type: [Number], required: true },
+	weeklyPoints: { type: ExercisesPointsSchema, required: true },
+	experience: { type: Number, required: true },
+	currentLeague: { type: Number, required: true },
 });
 const User = mongoose.model("User", UserSchema);
 
+const LeagueSchema = new mongoose.Schema({
+	users: { type: [UserSchema], required: true },
+	name: { type: String, required: true, unique: true },
+	range: { type: [Number], required: true, unique: true },
+});
+const League = mongoose.model("League", LeagueSchema);
+
+const LeaguesSchema = new mongoose.Schema({
+	leagues: { type: [LeagueSchema], required: true },
+});
+const Leagues = mongoose.model("Leagues", LeaguesSchema);
+
 // SignUp Endpoint (Create User)
 app.post("/signup", async (req, res) => {
-  const { name, username, email, password } = req.body;
+	const { name, username, email, password } = req.body;
 
-  const userExists = await User.findOne({ $or: [{ username }, { email }] });
-  if (userExists) {
-    return res.status(409).json({ message: "Username or email already registered." });
-  }
+	const userExists = await User.findOne({ $or: [{ username }, { email }] });
+	if (userExists) {
+		return res
+			.status(409)
+			.json({ message: "Username or email already registered." });
+	}
 
-  const newUser = new User({ name, username, email, password });
-  await newUser.save();
-  res.status(201).json(newUser);
+	const newUser = new User({ name, username, email, password });
+	await newUser.save();
+	res.status(201).json(newUser);
 });
 
 // Login Endpoint (Authenticate User)
 app.post("/login", async (req, res) => {
-  const { username, password } = req.body;
+	const { username, password } = req.body;
 
-  const user = await User.findOne({ $or: [{ username }, { email: username }] });
-  if (user && user.password === password) {
-    res.status(200).json({ message: "Login successful", user });
-  } else {
-    res.status(401).json({ message: "Invalid username or password" });
-  }
+	const user = await User.findOne({
+		$or: [{ username }, { email: username }],
+	});
+	if (user && user.password === password) {
+		res.status(200).json({ message: "Login successful", user });
+	} else {
+		res.status(401).json({ message: "Invalid username or password" });
+	}
 });
 
-app.use(cors({
-    origin: 'http://localhost:5173'
-  }));
+app.use(
+	cors({
+		origin: "http://localhost:5173",
+	})
+);
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+	console.log(`Server is running on http://localhost:${PORT}`);
 });
